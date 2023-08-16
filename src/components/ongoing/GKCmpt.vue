a72c8b17761b9bef7db399c74a9ea599625210dc
JYHyym <15764302140@163.com>
2023-08-16 17:16:24 +0800

    feat:加入登录按钮

--- src/components/ongoing/GKCmpt.vue
+++ src/components/ongoing/GKCmpt.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { genFileId } from 'element-plus'
 import type { UploadInstance, UploadProps, UploadRawFile } from 'element-plus'
+import { ElNotification } from 'element-plus'
 
 import students  from '@/data/accounts.js'
 // import login from '@/testFunctions/login.js'
@@ -10,6 +11,7 @@ import apis from '@/apis/index.ts'
 const tableData = ref(students)
 tableData.value.forEach((element: any, index:number) => {
   element.index = index + 1
+  element.loginBtnDis = false
 })
 
 const handleClick = async (index: number, row: any) => {
@@ -23,10 +25,40 @@ const handleClick = async (index: number, row: any) => {
     }})
     console.log(res)
   } catch (error) {
-    
+    console.log(error)
   }
   
 }
+
+// 账号登录
+const loginClick = async (index: number, row: any) => {
+  // runSelenium()
+  // login(row.link, row.account, row.psw)
+  try {
+    const res = await apis.linkInfo.postLogin({query: {
+      link: row.link,
+      account: row.account,
+      psw: row.psw
+    }})
+    if(res?.data.code === 1) {
+      ElNotification.success({
+        title: '提示',
+        message: '账号登录成功！',
+        showClose: false,
+      })
+
+      // 登录成功后登录按钮禁止点击
+      tableData.value[index].loginBtnDis = true
+    }
+    console.log(tableData.value, row, res)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const startClick = async (index: number, row: any) => {}
+
+const stopClick = async (index: number, row: any) => {}
 
 const upload = ref<UploadInstance>()
 
@@ -74,17 +106,25 @@ const submitUpload = () => {
       
     </div>
     
-
     <el-table :data="tableData" stripe height="calc(100vh - 80px)" style="width: 100%">
       <el-table-column prop="index" label="序号" width="80" />
-      <el-table-column prop="account" label="账号"  />
+      <el-table-column prop="account" label="姓名" />
+      <el-table-column prop="account" label="账号" />
       <el-table-column prop="psw" label="密码" />
       <el-table-column prop="link" label="url" />
       <el-table-column prop="school" label="学校" />
-      <el-table-column fixed="right" label="操作" width="120">
+      <el-table-column fixed="right" label="操作">
         <template #default="scope">
-          <el-button  size="small" @click="handleClick(scope.$index, scope.row)">操作</el-button>
-          <!-- <el-button link type="primary" size="small">Edit</el-button> -->
+          <!-- <el-button  size="small" @click="handleClick(scope.$index, scope.row)">操作</el-button> -->
+          <el-button 
+            link 
+            type="primary" 
+            size="small" 
+            @click="loginClick(scope.$index, scope.row)" 
+            :disabled="scope.row.loginBtnDis"
+          >登录</el-button>
+          <el-button link type="primary" size="small" @click="startClick(scope.$index, scope.row)">开始</el-button>
+          <el-button link type="primary" size="small" @click="stopClick(scope.$index, scope.row)">暂停</el-button>
         </template>
       </el-table-column>
     </el-table>
